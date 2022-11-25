fb8abcefe0a382c851e741ee922dddeeeb35e376
wlonestar <wlonestar@163.com>
2022-11-25 15:36:44 +0800

    dev: create empty spring boot project

---
+++ virosa-server/src/main/java/com/wjl/virosa/VirosaServerApplication.java
@@ -0,0 +1,13 @@
+package com.wjl.virosa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirosaServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(VirosaServerApplication.class, args);
+  }
+
+}
---
+++ virosa-server/src/test/java/com/wjl/virosa/VirosaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjl.virosa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirosaServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
